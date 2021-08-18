b89832d47310f65c4925c3c157d36d4aa63c377b
adimerpaul <adimer101@gmail.com>
2021-08-18 15:43:35 -0400

    Update quasar.conf.js

--- inmueblefront/quasar.conf.js
+++ inmueblefront/quasar.conf.js
@@ -73,7 +73,7 @@ module.exports = configure(function (ctx) {
       env: {
         API: ctx.dev
           ? 'http://localhost:8000/api'
-          : 'https://prod.api.com'
+          : 'http://192.168.154.208:85/api'
       }
     },
 
